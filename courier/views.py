dec3b83accc4af56454ca604c6a28d43499ea5e9
fred4impact <runtesting4me@gmail.com>
2021-03-14 23:11:10 +0000

    route update

--- courier/views.py
+++ courier/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='login')
 def courier(request):
     return render(request, 'courier/courier.html')
--- customer/urls.py
+++ customer/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from customer import views 
+app_name = "customer"
 
 urlpatterns = [
     path('', views.customer, name='customer'),    
