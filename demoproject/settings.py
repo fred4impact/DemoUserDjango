bf7d08f077c10cce46a756f9ec49d355a81ee135
fred4impact <runtesting4me@gmail.com>
2021-03-14 20:29:44 +0000

    updated envroments

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
+from decouple import config
+
 
 from pathlib import Path
 
@@ -20,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@rbo%b7s@v&e!#bv+_8sh%je(bntyvu05aw26upw+pgr6gtfj%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
+
 
 ALLOWED_HOSTS = []
 
